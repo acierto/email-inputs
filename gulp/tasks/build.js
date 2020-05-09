c83a9bbd236d9e91c395f11a6c4ded55bd00d70d
Bogdan Nechyporenko <bogdannechiporenko@gmail.com>
2020-05-09 18:36:22 +0200

    Circle CI (#1)
    Configured Circle CI

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,3 +3,5 @@ import gulp from 'gulp';
 gulp.task('build-gh-pages', gulp.series('lint-all', 'add-linters', 'webpack-gh-pages'));
 
 gulp.task('build-development', gulp.series('lint-all', 'add-linters', 'webpack-development'));
+
+gulp.task('build-production', gulp.series('lint-all', 'webpack-production'));
